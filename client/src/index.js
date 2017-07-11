902983a644b7445d8eb0ad3635ac7b30e344c284
John Kavanagh <john@invisiblearchitects.com>
2017-07-11 18:48:34 +0100

    init and add bootstrap

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import styles from 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
