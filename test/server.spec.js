c45179a80902831eb332775de1a18673b0270781
John Kavanagh <john@invisiblearchitects.com>
2017-07-15 19:24:09 +0100

    mocking search results - unfinished

--- test/server.spec.js
+++ test/server.spec.js
@@ -2,14 +2,17 @@ const chai = require('chai'),
       chaiHttp = require('chai-http'),
       should = chai.should(),
       fs = require('fs'),
-      moment = require('moment');
+      moment = require('moment'),
+      sinon = require('sinon'),
+      mockResults = require('./__mocks/results.json');
 
 
 process.env.APIKEY = 'ss630745725358065467897349852985';
+process.env.NODE_ENV = 'test';
 
 it('can start the server and get a response', function(done) {
 
-  const app = require('../server/src/server');
+  const app = require('../server/src/server').app;
   
   chai.use(chaiHttp);
   chai.request(app)
@@ -24,7 +27,7 @@ it('can start the server and get a response', function(done) {
 
 it('connects to the api and gets a validation error', function(done) {
 
-  const app = require('../server/src/server');
+  const app = require('../server/src/server').app;
   
   chai.use(chaiHttp);
   chai.request(app)
@@ -42,17 +45,24 @@ it('connects to the api and gets a validation error', function(done) {
 
 it('connects to the api and gets results', function(done) {
   this.timeout(5000);
-  const app = require('../server/src/server');
-  let  OutboundDate = moment().format('YYYY-MM-DD');
+  const server = require('../server/src/server');
+  const app = server.app, api = server.api;
+  api.livePricing.search = sinon.stub();
+  api.livePricing.search.returns(Promise.resolve(JSON.parse(mockResults)))
+  let  outboundDate = moment().add(1,'months').format('YYYY-MM-DD');
+  let  inboundDate = moment().add(2,'months').format('YYYY-MM-DD');
   chai.use(chaiHttp);
   chai.request(app)
- 	    .get(`/api/search?fromPlace=edi&toPlace=stn&fromDate=${OutboundDate}&class=Economy`)
- 	    .end(function(err, res) {        
+ 	    .get(`/api/search?fromPlace=edi&toPlace=syd&fromDate=${outboundDate}&toDate=${inboundDate}&class=Economy`)
+ 	    .then(res=> {
  	      res.should.have.status(200);
         res.body.should.not.be.empty;
         res.body.Status.should.equal('UpdatesComplete');
         res.body.Agents.should.exist;
  	      app.close();
-        done()
- 	    });
+        done();
+ 	    })
+      .catch(e=>{
+        console.error(Object.keys(e),e.response)
+      });
 });
